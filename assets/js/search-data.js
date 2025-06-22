7965e27f98eff06a97990ba842ef7919db046645
Bibek-Luitel <bibek.luitel@outlook.com>
2025-06-22 01:42:17 +0000

    Deploying to gh-pages from @ Bibek-Luitel/bibek-luitel.github.io@1fae10fb72799b235525fb2f21f054ef36e5e967 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -542,25 +542,11 @@ ninja.data = [{
           window.open("https://www.researchgate.net/profile/Bibek-Luitel-4/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=L8h9lIIAAAAJ&hl", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
