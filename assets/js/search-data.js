6da222f8993d216ce74387ac40213fb9f89cad14
Bibek-Luitel <bibek.luitel@outlook.com>
2025-06-22 00:37:18 +0000

    Deploying to gh-pages from @ Bibek-Luitel/bibek-luitel.github.io@613ae2859541c4f4e73ea5304dadd314540737ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -525,7 +525,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=L8h9lIIAAAAJ&hl", "_blank");
         },
       },{
         id: 'social-custom_social',
