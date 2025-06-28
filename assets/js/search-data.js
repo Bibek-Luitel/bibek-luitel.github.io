65ca1c9697815e9905a0796d91970c918cdf16ab
Bibek-Luitel <bibek.luitel@outlook.com>
2025-06-28 17:22:00 +0000

    Deploying to gh-pages from @ Bibek-Luitel/bibek-luitel.github.io@946453a279836d125e19dc290d308e91625a0166 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,8 +433,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-attened-pathways-to-profession-p2p-symposium-held-in-tarleton-state-university-sponsored-by-tamus",
-          title: 'Attened Pathways to Profession (P2P) Symposium held in Tarleton State University sponsored by...',
+            },},{id: "news-attened-pathways-to-profession-p2p-symposium-at-tarleton-state-university-sponsored-by-tamus",
+          title: 'Attened Pathways to Profession (P2P) Symposium at Tarleton State University sponsored by TAMUS....',
           description: "",
           section: "News",},{id: "news-i-advanced-to-candidacy-and-i-am-a-doctoral-candidate-man-student",
           title: 'I advanced to candidacy and I am a doctoral candidate :man_student:',
