11289c8cb8cd9fd419c679444b03703db7600b59
Bibek-Luitel <bibek.luitel@outlook.com>
2025-07-09 20:00:05 +0000

    Deploying to gh-pages from @ Bibek-Luitel/bibek-luitel.github.io@32b2f0ec0035e3ea664d0c2ed2792a874c52a3bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -138,12 +138,12 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-grocery-sales-forecasting",
           title: 'Grocery Sales Forecasting',
-          description: "with background image",
+          description: "Multivariate analysis of grocery wholesale sales using MANOVA, factor analysis, PCA, and clustering techniques.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-scholarship-prediction-ml-model",
           title: 'Scholarship Prediction ML Model',
-          description: "a project with a background image and giscus comments",
+          description: "Binary classification of scholarship eligibility using SVM, logistic regression, and neural networks.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-5",
