04d2f17e19ce0b6fe7f57ff068c1957e35e57ca9
Bibek-Luitel <bibek.luitel@outlook.com>
2025-07-10 03:36:11 +0000

    Deploying to gh-pages from @ Bibek-Luitel/bibek-luitel.github.io@b648edc2198347451419aa17030553001a9b01dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,7 +121,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_story_of_my_experiments_with_truth/";
-            },},{id: "news-attened-pathways-to-profession-p2p-symposium-at-tarleton-state-university-sponsored-by-tamus",
+            },},{id: "news-microphone-presentated-my-research-at-the-star-global-confernce-in-kathmendu-nepal",
+          title: ':microphone:Presentated my research at the STAR Global Confernce in Kathmendu, Nepal.',
+          description: "",
+          section: "News",},{id: "news-attened-pathways-to-profession-p2p-symposium-at-tarleton-state-university-sponsored-by-tamus",
           title: 'Attened Pathways to Profession (P2P) Symposium at Tarleton State University sponsored by TAMUS....',
           description: "",
           section: "News",},{id: "news-microphone-presented-my-research-at-the-sera-annual-conference-in-san-antonio-texas",
